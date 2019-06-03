5cff97a900283ff2be9198a578090213dd60f5e0
Marvin <amazingmarvinapp@gmail.com>
2019-06-03 20:06:10 +0200

    Add dist files to repo.

---
+++ dist/esm/src/datewithzone.js
@@ -0,0 +1,33 @@
+import dateutil from './dateutil';
+var DateWithZone = /** @class */ (function () {
+    function DateWithZone(date, tzid) {
+        this.date = date;
+        this.tzid = tzid;
+    }
+    Object.defineProperty(DateWithZone.prototype, "isUTC", {
+        get: function () {
+            return !this.tzid || this.tzid.toUpperCase() === 'UTC';
+        },
+        enumerable: true,
+        configurable: true
+    });
+    DateWithZone.prototype.toString = function () {
+        var datestr = dateutil.timeToUntilString(this.date.getTime(), this.isUTC);
+        if (!this.isUTC) {
+            return ";TZID=" + this.tzid + ":" + datestr;
+        }
+        return ":" + datestr;
+    };
+    DateWithZone.prototype.getTime = function () {
+        return this.date.getTime();
+    };
+    DateWithZone.prototype.rezonedDate = function () {
+        if (this.isUTC) {
+            return this.date;
+        }
+        return this.date;
+    };
+    return DateWithZone;
+}());
+export { DateWithZone };
+//# sourceMappingURL=datewithzone.js.map
